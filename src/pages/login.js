b5d47776d99dd2d9906cee06ac97d7041b53805f
Elisson Victor <138872302+EVCG@users.noreply.github.com>
2024-10-16 16:37:47 -0300

    alteracoes

---
+++ src/pages/login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '../styles/login.module.css'; 
+import logo from '../img/logo.png';
+import logo2 from '../img/logo2.png';
+import Image from "next/image";
+
+export default function Login() {
+  return (
+  
+      <div className={styles.login_container}>
+        <div className={styles.login_box}>
+          <div className={styles.logo_container}>
+            <Image src={logo} alt="CEUB Logo" className={styles.logo} />
+          </div>
+          <form className={styles.login_form}>
+            <input type="email" placeholder="E-mail" className={styles.input_field} />
+            <input type="password" placeholder="Senha" className={styles.input_field} />
+            <button type="submit" className={styles.login_button}>ENTRAR</button>
+            <a href="#" className={styles.forgot_password}>Esqueci minha senha</a>
+          </form>
+          <div className={styles.logo_container2}>fdasfcsadcdascsd
+            {/* <Image src={logo2} alt="CEUB Logo" className={styles.logo} /> */}
+          </div>
+        </div>
+      </div>
+  );
+}
---
+++ src/pages/page.js
@@ -0,0 +1,151 @@
+import styles from "../styles/page.module.css";
+import friends from "../img/friends.jpg";
+import livro from "../img/livro.jpg";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className={styles.body}>
+      {/* Cabeçalho */}
+      <div className={styles.header}>
+        <div className={styles.logo_principal}>
+          <img
+            src="https://institucional.uniceub.br/hubfs/BrandCenter/img/logo-ceub-colorida-02.png"
+            className={styles.logo}
+          ></img>
+        </div>
+
+        {/* Navegação do site */}
+        <div className={styles.nav}>
+          <a href="#" className={styles.nav_painel}>Painel</a>
+          <a href="#" className={styles.nav_base_profesores}>Base Professores</a>
+          <a href="#" className={styles.nav_manual}>Manual</a>
+        </div>
+
+        {/* Usuario */}
+        <div className={styles.usuario}>
+          <h1>SEU NOME</h1>
+          <div className={styles.avatar}>
+            <div className={styles.cabeca}></div>
+            <div className={styles.corpo}></div>
+          </div>
+        </div>
+      </div>
+
+      {/* Conteudo Principal do site */}
+      <div className={styles.main}>
+        {/* Area de recepção e anuncios */}
+        <div className={styles.recepcao}>
+          <div className={styles.apresentacao}>
+            <span>Olá *Seu Nome*,</span><br></br>
+            <span>Seja Bem-Vindo(a) ao</span><br></br>
+            <span className={styles.mensagem_organizador}>Organizador</span>
+
+            <button className={styles.tutorial}><img src={livro}></img>Tutorial</button>
+          </div>
+          {/* Mural de alertas */}
+          <div className={styles.anuncio}>
+            <div className={styles.quadro_alerta}>
+              <div className={styles.tituloMural}>
+                <span><img src="#"></img></span>
+                <h2>Mural de Alertas</h2>
+              </div>
+              <p>Aqui aparecerá as turmas que estão sem professores definidos.</p>
+
+              <div className={styles.lista_alerta}>
+                <AlertItem />
+                <AlertItem />
+                <AlertItem />
+                <AlertItem />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.container}>
+          {/* Filtros */}
+          <div className={styles.filtros}>
+            <select>
+              <option>Curso</option>
+              <option>Direito</option>
+              <option>Medicina</option>
+              <option>Psicologia</option>
+            </select>
+
+            <select>
+              <option>Semestres</option>
+              <option>1º Semestre</option>
+              <option>2º Semestre</option>
+              <option>3º Semestre</option>
+            </select>
+
+            <select>
+              <option>Turno</option>
+              <option>Matutino</option>
+              <option>Vespertino</option>
+              <option>Noturno</option>
+            </select>
+
+            <input type="text" placeholder="Buscar" className={styles.buscar} />
+          </div>
+
+          {/* Grid de Cards */}
+          <div className={styles.grade_cartoes}>
+            {Array.from({ length: 12 }, (_, i) => (
+              <Cartao key={i} />
+            ))}
+          </div>
+
+          {/* Paginação */}
+          <div className={styles.paginacao}>
+            <span><img className={styles.esquerda} src="https://cdn-icons-png.flaticon.com/512/109/109617.png"></img>  Anterior</span>
+            <a href="#" className={styles.numero1}>1</a>
+            <a href="#" className={styles.numero2}>2</a>
+            <a href="#" className={styles.numero3}>3</a>
+            <a href="#" className={styles.numeros}>...</a>
+            <a href="#" className={styles.numero7}>7</a>
+            <a href="#" className={styles.numero8}>8</a>
+            <span>Próximo  <img className={styles.direita} src="https://cdn-icons-png.flaticon.com/512/109/109617.png"></img></span>
+          </div>
+        </div>
+      </div>
+
+      {/* Rodapé */}
+      <div className={styles.footer}></div>
+    </div>
+  );
+}
+
+function AlertItem() {
+  return (
+    <div className={styles.lista_alerta}>
+      <div className={styles.esquerda_alerta_item}>
+        <div className={styles.turmaResumida}>
+          <div className={styles.icone_alerta}>!</div>
+          <div className={styles.text_alerta}>Turma Resumida</div>
+        </div>
+        <button className={styles.botao_professor}><Image src={friends} className={styles.friends}/> Professores</button>
+      </div>
+    </div>
+  );
+}
+
+function Cartao() {
+  return (
+    <div className={styles.cartao}>
+      <div className={styles.conteudo_cartao}>
+        <div className={styles.cabecalho_cartao}>
+          <div className={styles.icone} />
+        </div>
+        <div className={styles.corpo_cartao}>
+          <h3>Nome do Curso</h3>
+          <p>Semestre</p>
+          <p>Matutino</p>
+        </div>
+      </div>
+      <div className={styles.rodape_cartao}>
+        <span className={styles.status}>Resolvido</span>
+      </div>
+    </div>
+  );
+}
