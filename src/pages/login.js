13ddb847015e75e0663cd9d09ac37e5c5673ea39
Elisson Victor <138872302+EVCG@users.noreply.github.com>
2024-10-31 17:27:29 -0300

    atualização

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../components/login.module.css'; 
+import styles from '../styles/login.module.css'; 
 import logo from '../img/logo.png';
 import Image from "next/image";
 
